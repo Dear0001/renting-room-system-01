c822bbb6ebbd2004af44d3d1f51c3b002840e612
seyha-rouge <penhseyha4980@gmail.com>
2024-07-06 10:06:26 +0700

    fix edi and show

--- resources/views/categories/edit.blade.php
+++ resources/views/categories/edit.blade.php
@@ -8,11 +8,11 @@
             @method('PUT')
             <div class="form-group">
                 <label for="category_name">Category Name</label>
-                <input type="text" name="category_name" id="category_name" class="form-control" value="{{ $category->category_name }}" required>
+                <input type="text" name="category_name" id="category_name" class="form-control" value="{{ $category->name }}" required>
             </div>
             <div class="form-group">
                 <label for="category_description">Description</label>
-                <textarea name="category_description" id="category_description" class="form-control">{{ $category->category_description }}</textarea>
+                <textarea name="category_description" id="category_description" class="form-control">{{ $category->description }}</textarea>
             </div>
             <button type="submit" class="btn btn-primary">Update Category</button>
         </form>
